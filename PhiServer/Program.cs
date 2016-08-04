79b0cb5b45b6af2ab8997ec2acd03fca9a037f19
Longwelwind <longwelwind@gmail.com>
2016-08-04 18:17:32 +0200

    Makes the whole packet processing mono-threaded for the server

--- PhiServer/Program.cs
+++ PhiServer/Program.cs
@@ -3,6 +3,7 @@ using System.Net;
 using SocketLibrary;
 using PhiClient;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace PhiServer
 {
@@ -11,6 +12,8 @@ namespace PhiServer
         private Server server;
         private RealmData realmData;
         private Dictionary<ServerClient, User> connectedUsers = new Dictionary<ServerClient, User>();
+
+        private object lockProcessPacket = new object();
 
         public void Start(IPAddress ipAddress, int port)
         {
@@ -67,66 +70,68 @@ namespace PhiServer
 
         private void MessageCallback(ServerClient client, byte[] data)
         {
-            Packet packet = Packet.Deserialize(data, this.realmData);
-            Console.WriteLine("Received packet " + packet);
-
-            User user;
-            this.connectedUsers.TryGetValue(client, out user);
-
-            if (packet is AuthentificationPacket)
+            lock (lockProcessPacket)
             {
-                // Special packets, (first sent from the client)
-                AuthentificationPacket authPacket = (AuthentificationPacket)packet;
+                Packet packet = Packet.Deserialize(data, this.realmData);
+                Console.WriteLine("Received packet " + packet);
 
-                // We first check if the version corresponds
-                if (authPacket.version != RealmData.VERSION)
+                User user;
+                this.connectedUsers.TryGetValue(client, out user);
+
+                if (packet is AuthentificationPacket)
                 {
-                    this.SendPacket(client, new AuthentificationErrorPacket
+                    // Special packets, (first sent from the client)
+                    AuthentificationPacket authPacket = (AuthentificationPacket)packet;
+
+                    // We first check if the version corresponds
+                    if (authPacket.version != RealmData.VERSION)
+                    {
+                        this.SendPacket(client, new AuthentificationErrorPacket
                         {
-                            error = "Server is version " + RealmData.VERSION  + " but client is version " + authPacket.version
+                            error = "Server is version " + RealmData.VERSION + " but client is version " + authPacket.version
                         }
-                    );
-                    return;
-                }
+                        );
+                        return;
+                    }
 
-                // We check if an user already uses this key
-                user = this.realmData.users.FindLast(delegate (User u) { return u.hashedKey == authPacket.hashedKey; });
-                if (user == null)
-                {
-                    user = this.realmData.ServerAddUser(authPacket.name, authPacket.hashedKey);
-                    user.connected = true;
+                    // We check if an user already uses this key
+                    user = this.realmData.users.FindLast(delegate (User u) { return u.hashedKey == authPacket.hashedKey; });
+                    if (user == null)
+                    {
+                        user = this.realmData.ServerAddUser(authPacket.name, authPacket.hashedKey);
+                        user.connected = true;
 
-                    Console.WriteLine("Creating user \"" + user.name + "\" with ID " + user.getID());
+                        Console.WriteLine("Creating user \"" + user.name + "\" with ID " + user.getID());
 
-                    // We send a notify to all users connected about the new user
-                    this.realmData.BroadcastPacketExcept(new NewUserPacket { user = user }, user);
+                        // We send a notify to all users connected about the new user
+                        this.realmData.BroadcastPacketExcept(new NewUserPacket { user = user }, user);
+                    }
+                    else
+                    {
+                        // We send a connect notification to all users
+                        user.connected = true;
+                        this.realmData.BroadcastPacketExcept(new UserConnectedPacket { user = user, connected = true }, user);
+                    }
+
+                    this.connectedUsers.Add(client, user);
+
+
+                    // We respond with a StatePacket that contains all synchronisation data
+                    this.SendPacket(client, new SynchronisationPacket { user = user, realmData = this.realmData });
                 }
                 else
                 {
-                    // We send a connect notification to all users
-                    user.connected = true;
-                    this.realmData.BroadcastPacketExcept(new UserConnectedPacket { user = user, connected = true }, user);
+                    if (user == null)
+                    {
+                        // We ignore this package
+                        Console.WriteLine("Ignore packet because unknown user");
+                        return;
+                    }
+
+                    // Normal packets, we defer the execution
+                    packet.Apply(user, this.realmData);
                 }
-
-                this.connectedUsers.Add(client, user);
-
-
-                // We respond with a StatePacket that contains all synchronisation data
-                this.SendPacket(client, new SynchronisationPacket { user = user, realmData = this.realmData });
             }
-            else
-            {
-                if (user == null)
-                {
-                    // We ignore this package
-                    Console.WriteLine("Ignore packet because unknown user");
-                    return;
-                }
-
-                // Normal packets, we defer the execution
-                packet.Apply(user, this.realmData);
-            }
-
         }
 
         static void Main(string[] args)
